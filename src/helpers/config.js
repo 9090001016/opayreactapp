d9657c2d46eaf7c8c656e6b0c84a47b9f0022f6d
kalpesh.gohil <kalpesh.gohil@BVIMUMLAP-91>
2021-06-29 21:03:38 +0530

    APIURL change.

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,8 @@
 let config = {
   // apiUrl: "https://localhost:44321/api/",
   // apiUrl: "https://k2api.dcdev.brainvire.net/api/",
-  apiUrl: "https://instant-dev-webapi.azurewebsites.net/api/",
+  // apiUrl: "https://instant-dev-webapi.azurewebsites.net/api/",
+  apiUrl: "https://onepay-dev-webapi.azurewebsites.net/api/",
 
   instantApiUrl: "https://instant-dev-mrchntwebapi.azurewebsites.net/api/",
   onePayApiUrl: "https://onepay-dev-mrchntwebapi.azurewebsites.net/api/",
