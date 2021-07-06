57f8269a9eb726c59014945bc863f4a1a15dfd46
Suraj Kumthekar <suraj.kumthekar@brainvire.com>
2021-07-06 14:36:40 +0000

    Updated config.js

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,11 +2,11 @@ let config = {
   // apiUrl: "https://localhost:44321/api/",
   // apiUrl: "https://k2api.dcdev.brainvire.net/api/",
   // apiUrl: "https://instant-dev-webapi.azurewebsites.net/api/",
-  apiUrl: "https://onepay-dev-webapi.azurewebsites.net/api/",
+  apiUrl: "https://opayapi.brainvire.net/api/",
   //apiUrl: "https://api.onepay.ai/api/",
 
-  instantApiUrl: "https://instant-dev-mrchntwebapi.azurewebsites.net/api/",
-  onePayApiUrl: "https://onepay-dev-mrchntwebapi.azurewebsites.net/api/",
+  instantApiUrl: "https://instmrcapi.brainvire.net/api/",
+  onePayApiUrl: "https://opaymrcapi.brainvire.net/api/",
 };
 
 export default config;
