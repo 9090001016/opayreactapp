af6ea3050516b86f08658957de9c84b3545d5249
Deepak Savani <deepak.savani@brainvire.com>
2021-10-22 20:43:46 +0530

    working on redireact url in onepay form.

---
+++ Dev/src/helpers/config.js
@@ -0,0 +1,12 @@
+let config = {
+  apiUrl: "https://localhost:44321/api/",
+  // apiUrl: "https://k2api.dcdev.brainvire.net/api/",
+  // apiUrl: "https://instant-dev-webapi.azurewebsites.net/api/",
+  // apiUrl: "https://opayapi.brainvire.net/api/",
+  //apiUrl: "https://api.onepay.ai/api/",
+
+  instantApiUrl: "https://instmrcapi.brainvire.net/api/",
+  onePayApiUrl: "https://opaymrcapi.brainvire.net/api/",
+};
+
+export default config;
